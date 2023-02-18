2635575ec859deef40a6ae2cbd5d32d4085fa7b9
DavydovQAA <ghostinside80@gmail.com>
2023-02-18 19:21:53 +0300

    Third commit

---
+++ tests/test_buy_product.py
@@ -0,0 +1,21 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.login_page import Login_page
+from pages.main_page import Main_page
+
+
+def test_buy_product():
+    driver = webdriver.Chrome()
+
+    print("Start Test")
+
+    login = Login_page(driver)
+    login.authorization()
+    mp = Main_page(driver)
+    mp.select_product()
+
+
